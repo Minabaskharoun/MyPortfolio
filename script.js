c7bf4cd2e424e7be208bc2072db33e18b8e6ac0e
Minabaskharoun <78620169+Minabaskharoun@users.noreply.github.com>
2021-02-25 21:25:07 -0500

    myPortfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById("btn-project2");
+
+function redirectFunction() {
+    window.location.href = "https://minabaskharoun.github.io/projectWeek2/";
+}
+
+btn.onclick = redirectFunction;
